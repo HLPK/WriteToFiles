3012bac46298cb723f118242c2c08e6519e43f08
Галина Павлюк <gp0407@gmail.com>
2021-04-26 22:50:52 +0300

    Добавьте файлы проекта.

---
+++ WriteToFiles/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WriteToFiles
+{
+    class Program
+    {
+        static readonly object _locker = new object();
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main thread id: {Thread.CurrentThread.GetHashCode()}");
+            Thread thread1 = new Thread(ManageFile1);
+            thread1.Start();
+
+            Thread thread2 = new Thread(ManageFile2);
+            thread2.Start();
+
+            Console.ReadKey();
+        }
+
+        private static void ManageFile1()
+        {
+            Console.WriteLine($"Current thread id: {Thread.CurrentThread.ManagedThreadId}");
+
+            StreamReader streamReader = File.OpenText("C:\\csharp\\File1.txt");
+            string text = streamReader.ReadToEnd();
+            Console.WriteLine("File1 reading: "+text);
+            
+            lock (_locker)
+            {
+                StreamWriter streamWriter = new StreamWriter("C:\\csharp\\File3.txt",true);
+                Console.WriteLine("File3 writing: " + text);
+                streamWriter.WriteLine(text);
+                streamWriter.Close();
+            }
+        }
+
+        private static void ManageFile2()
+        {
+            Console.WriteLine($"Current thread id: {Thread.CurrentThread.ManagedThreadId}");
+
+            StreamReader streamReader = File.OpenText("C:\\csharp\\File2.txt");
+            string text = streamReader.ReadToEnd();
+            Console.WriteLine("File2 reading: " + text);
+            
+            lock (_locker)
+            {
+                StreamWriter streamWriter = new StreamWriter("C:\\csharp\\File3.txt",true);
+                Console.WriteLine("File3 writing: " + text);
+                streamWriter.WriteLine(text);
+                streamWriter.Close();
+            }
+        }
+    }
+}
